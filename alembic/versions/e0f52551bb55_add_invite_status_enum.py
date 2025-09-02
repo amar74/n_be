0f52c4bd5da580baf31965e5efbb925242844a4b
Rudeus <prathamkamthan1306@gmail.com>
2025-09-02 19:50:40 +0530

    fix: removed 2 heads error and streamlined alembic history

--- alembic/versions/e0f52551bb55_add_invite_status_enum.py
+++ alembic/versions/e0f52551bb55_add_invite_status_enum.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 
 revision: str = 'e0f52551bb55'
-down_revision: Union[str, None] = 'bf6419186bf8'
+down_revision: Union[str, None] = '86256bdba01e'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
