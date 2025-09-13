0d9bdbed88983497b78d22b061b6a7ca67360383
Mohan Gautam <mohanu526@gmail.com>
2025-09-13 17:13:30 +0530

    dummy commit

--- client/src/pages/CreateOrganizationPage/CreateOrganizationPage.tsx
+++ client/src/pages/CreateOrganizationPage/CreateOrganizationPage.tsx
@@ -107,7 +107,7 @@ function CreateOrganizationPage() {
                         </div>
                         <FormControl>
                           <div className="relative">
-                            <Globe className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
+                            <Globe className="absolute  left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
                             <Input
                               {...field}
                               type="url"
